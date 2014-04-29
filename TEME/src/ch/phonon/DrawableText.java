9a1f71aaa1ae59c1a619774c6a84e67c7cd62989
phonon <StephanStrauss@gmx.ch>
2014-04-29 22:15:24 +0200

    Solved somehow (It is maybe a not good choice - best to check with Oracle) to obtain  width and height of the font without having a Graphics2D reference. Therefore I can keep it  in the constructor of the DrawableText.

--- TEME/src/ch/phonon/DrawableText.java
+++ TEME/src/ch/phonon/DrawableText.java
@@ -3,9 +3,11 @@
  */
 package ch.phonon;
 
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.Shape;
 import java.awt.font.FontRenderContext;
+import java.awt.font.TextAttribute;
 import java.awt.font.TextLayout;
 import java.awt.geom.AffineTransform;
 import java.text.AttributedCharacterIterator;
@@ -22,7 +24,20 @@ public class DrawableText extends AbstractDrawable {
 		
 		super(starpoint, localOrientation);
 		this.aString = text;
-				
+		
+		//TODO: 	This way to obtain the font size is not precise ...
+		//			Still I think, there should be a better way
+		// http://stackoverflow.com/questions/4914145/getting-string-size-in-java-without-having-a-graphics-object-available
+		
+		Font font = (Font)(aString.getIterator().getAttribute(TextAttribute.FONT));
+		FontRenderContext frc = new FontRenderContext(font.getTransform(), true, true);
+		AttributedCharacterIterator aci = aString.getIterator();
+		TextLayout textLayout = new TextLayout(aci,frc);
+		Shape outLine = textLayout.getOutline(null);
+		this.height = outLine.getBounds2D().getHeight();
+		this.width= outLine.getBounds2D().getWidth();
+		
+		
 	}
 	
 	@Override
